de9e63560a2ae927d9f54340199d7c32f44fa583
lucjaan <luka.pietrzykowski@gmail.com>
2024-01-15 00:42:35 +0100

    WIP Camera system

--- RobotSimulatorApp/Form1.Designer.cs
+++ RobotSimulatorApp/Form1.Designer.cs
@@ -33,10 +33,15 @@ namespace RobotSimulatorApp
         private void InitializeComponent()
         {
             glControl = new OpenTK.WinForms.GLControl();
+            captureMouseCheckBox = new CheckBox();
+            releaseMouseTextBox = new TextBox();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // glControl
             // 
+            this.glControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             glControl.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
             glControl.APIVersion = new Version(3, 3, 0, 0);
             glControl.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
@@ -47,21 +52,56 @@ namespace RobotSimulatorApp
             glControl.SharedContext = null;
             glControl.Size = new Size(609, 424);
             glControl.TabIndex = 0;
-            glControl.Text = "glControl1";
+            glControl.Text = "glControl";
+            // 
+            // captureMouseCheckBox
+            // 
+            captureMouseCheckBox.AutoSize = true;
+            captureMouseCheckBox.Location = new Point(658, 359);
+            captureMouseCheckBox.Name = "captureMouseCheckBox";
+            captureMouseCheckBox.Size = new Size(98, 19);
+            captureMouseCheckBox.TabIndex = 1;
+            captureMouseCheckBox.Text = "Move camera";
+            captureMouseCheckBox.UseVisualStyleBackColor = true;
+            captureMouseCheckBox.CheckedChanged += captureMouseCheckBox_CheckedChanged;
+
+            // 
+            // releaseMouseTextBox
+            // 
+            releaseMouseTextBox.Location = new Point(658, 384);
+            releaseMouseTextBox.Name = "releaseMouseTextBox";
+            releaseMouseTextBox.Size = new Size(155, 23);
+            releaseMouseTextBox.TabIndex = 2;
+            releaseMouseTextBox.Text = "Press ESC to release mouse";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(642, 68);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(226, 213);
+            textBox1.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(892, 457);
+            Controls.Add(textBox1);
+            Controls.Add(releaseMouseTextBox);
+            Controls.Add(captureMouseCheckBox);
             Controls.Add(glControl);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private OpenTK.WinForms.GLControl glControl;
+        private CheckBox captureMouseCheckBox;
+        private TextBox releaseMouseTextBox;
+        private TextBox textBox1;
     }
 }
--- RobotSimulatorApp/Form1.cs
+++ RobotSimulatorApp/Form1.cs
@@ -1,39 +1,48 @@
+using Microsoft.VisualBasic.Devices;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
+using OpenTK.WinForms;
 using RobotSimulatorApp.GlConfig;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Reflection;
+using System.Drawing;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RobotSimulatorApp
 {
     public partial class Form1 : Form
     {
-        int VertexBufferObject;
-        int ElementBufferObject;
-        int VertexArrayObject;
-        int PositionBuffer;
-        int ColorBuffer;
+        private int ElementBufferObject;
+        private int VertexArrayObject;
+        private int PositionBuffer;
+        private int ColorBuffer;
+        readonly private float AspectRatio;
         private Shader? shader;
         private Timer timer = null!;
         private float _angle = 0.0f;
+        INativeInput? NativeInput;
+        Rectangle GlControlBounds;
+        bool FirstMove = true;
+        Camera? camera;
+        Vector3 front = new Vector3(0.0f, 0.0f, -1.0f);
+        Vector3 position = new Vector3(15f, 5.0f, 30.0f);
 
-        public int[] IndexData =
-        [
+
+        private static readonly int[] IndexData =
+        {
              0,  1,  2,  2,  3,  0,
              4,  5,  6,  6,  7,  4,
              8,  9, 10, 10, 11,  8,
             12, 13, 14, 14, 15, 12,
             16, 17, 18, 18, 19, 16,
             20, 21, 22, 22, 23, 20,
-        ];
+        };
 
         public Form1()
         {
             InitializeComponent();
+            AspectRatio = Math.Max(glControl.ClientSize.Width, 1) / (float)Math.Max(glControl.ClientSize.Height, 1);
         }
 
         protected override void OnLoad(EventArgs e)
@@ -41,6 +50,21 @@ namespace RobotSimulatorApp
             base.OnLoad(e);
             //glControl.Paint += glControl_Paint;
             SetUpOpenGL();
+
+            glControl.GotFocus += (sender, e) =>
+                textBox1.AppendText("Focus in");
+            glControl.LostFocus += (sender, e) =>
+                textBox1.AppendText("Focus out");
+
+
+            glControl.MouseDown += (sender, e) =>
+            {
+                glControl.Focus();
+                textBox1.AppendText($"WinForms Mouse down: ({e.X},{e.Y})");
+            };
+            glControl.KeyDown += (sender, e) =>
+                textBox1.AppendText($"WinForms Key down: {e.KeyCode}");
+
         }
 
         private static readonly Color4[] ColorData = new Color4[]
@@ -53,14 +77,11 @@ namespace RobotSimulatorApp
             Color4.ForestGreen, Color4.ForestGreen, Color4.ForestGreen, Color4.ForestGreen,
 };
 
-        public void SetUpOpenGL()
+        private void SetUpOpenGL()
         {
-            //glControl.Paint += glControl_Paint;
-
             GL.Enable(EnableCap.DepthTest);
 
-            Cube cube = new Cube("cube1", new Vector3(-1f, -1f, -1f), 2f, 2f, 2f);
-            Vector3[] verticesC = cube.ReturnInternalVectors();
+            Cube cube = new ("cube1", new Vector3(-1f, -1f, -1f), 2f, 2f, 2f);
 
             timer = new Timer();
             timer.Tick += (sender, e) =>
@@ -83,14 +104,7 @@ namespace RobotSimulatorApp
 
             PositionBuffer = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, PositionBuffer);
-            GL.BufferData(BufferTarget.ArrayBuffer, 3 * verticesC.Length * sizeof(float), verticesC, BufferUsageHint.StaticDraw);
-            //Matrix4 model = Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(time));
-            //creating a scene behind on z so we can see everything as origin point is 0,0,0
-            //Matrix4 view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
-            //creating perspective
-            //Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), Size.X / (float)Size.Y, 0.1f, 100.0f);
-            //Matrix4 model = Matrix4.CreateFromAxisAngle(new Vector3(0f, 1f, 0.0f), MathHelper.DegreesToRadians(70f));
-            //Matrix4 model = Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(_angle));
+            GL.BufferData(BufferTarget.ArrayBuffer, 3 * cube.ReturnInternalVectors().Length * sizeof(float), cube.ReturnInternalVectors(), BufferUsageHint.StaticDraw);
 
             shader = new Shader();
             shader.Use();
@@ -105,73 +119,165 @@ namespace RobotSimulatorApp
 
             GL.EnableVertexAttribArray(1);
             GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, sizeof(float) * 4, 0);
-            //shader = new Shader();
-            //shader.Use();
-
-
         }
 
-        public void Render()
+        private void Render()
         {
+
             glControl.MakeCurrent();
             GL.ClearColor(Color4.MidnightBlue);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.Enable(EnableCap.DepthTest);
 
+            position.Z = position.Z > 0 ? position.Z : 0;
+            position.Z = position.Z < 40 ? position.Z : 40;
+
+
             Matrix4 model = Matrix4.CreateFromAxisAngle(new Vector3(0.0f, 1.0f, 0.0f), MathHelper.DegreesToRadians(_angle));
 
             Matrix4 view = Matrix4.LookAt(0, 5, 5, 0, 0, 0, 0, 1, 0);
-            //Matrix4 view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
 
-            float aspect_ratio = Math.Max(glControl.ClientSize.Width, 1) / (float)Math.Max(glControl.ClientSize.Height, 1);
-            //Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 0.1f, 100f);
-            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, AspectRatio, 1, 64);
+
+            Matrix4 cameraView = Matrix4.LookAt(position, position + front, Vector3.UnitY);
+
 
 
             shader = new Shader();
             shader.Use();
-            shader.SetMatrix4("model", model);
-            shader.SetMatrix4("view", view);
-            shader.SetMatrix4("projection", projection);
 
+            camera = new(Vector3.UnitZ * 3, AspectRatio);
+
+            if (captureMouseCheckBox.Checked)
+            {
+                MoveCamera();
+            }
+
+            //var x = camera.GetViewMatrix();
+           // var y = camera.GetProjectionMatrix();
+
+            shader.SetMatrix4("model", model);
+            shader.SetMatrix4("view", cameraView);
+            //shader.SetMatrix4("view", camera.GetViewMatrix());
+            //shader.SetMatrix4("view", view);
+            shader.SetMatrix4("projection", projection);
+            //shader.SetMatrix4("projection", camera.GetProjectionMatrix());
 
             GL.DrawElements(BeginMode.Triangles, IndexData.Length, DrawElementsType.UnsignedInt, 0);
 
             glControl.SwapBuffers();
         }
 
-        public void CreateCube(string name, int baseA, int baseB, int height, int location)
+        private void MoveCamera()
         {
-            Debug.WriteLine("TODO");
-            //dasdasda
+            float Yaw = 0;
+            float Pitch = 0;
+            float Sensitivity = 0.1f;
+            INativeInput input = glControl.EnableNativeInput();
+
+            NativeInput.MouseMove += (e) =>
+            {
+                // Apply the camera pitch and yaw (we clamp the pitch in the camera class)
+                Yaw += e.DeltaX * Sensitivity;
+                Pitch -= e.DeltaY * Sensitivity; // Reversed since y-coordinates range from bottom to top
+                front.X = (float)Math.Cos(MathHelper.DegreesToRadians(Pitch)) * (float)Math.Cos(MathHelper.DegreesToRadians(Yaw));
+                front.Y = (float)Math.Sin(MathHelper.DegreesToRadians(Pitch));
+                front.Z = (float)Math.Cos(MathHelper.DegreesToRadians(Pitch)) * (float)Math.Sin(MathHelper.DegreesToRadians(Yaw));
+                front = Vector3.Normalize(front);
+                //Debug.Write($"{camera.Yaw}, {camera.Pitch} \n");
+            };
+
+            NativeInput.MouseWheel += (e) =>
+            {
+                if(e.OffsetY == -1 && position.Z > 0)
+                {
+                    position.Z -= 0.1f;
+                }
+
+                if (e.OffsetY == 1 && position.Z < 40)
+                {
+                    position.Z += 0.1f;
+                }
+
+                Debug.Write(position.Z + "\n");
+            };
+
+            NativeInput.KeyUp += (e) =>
+            {
+                if (e.Key == OpenTK.Windowing.GraphicsLibraryFramework.Keys.Home)
+                {
+                    front = new Vector3(0.0f, 0.0f, -1.0f);
+                }
+            };
+
+
+
         }
 
         private void glControl_Resize(object? sender, EventArgs e)
         {
             glControl.MakeCurrent();
-
+            
             if (glControl.ClientSize.Height == 0)
+            {
                 glControl.ClientSize = new System.Drawing.Size(glControl.ClientSize.Width, 1);
+            }
 
             GL.Viewport(0, 0, glControl.ClientSize.Width, glControl.ClientSize.Height);
 
-            //float aspect_ratio = Math.Max(glControl.ClientSize.Width, 1) / (float)Math.Max(glControl.ClientSize.Height, 1);
-            //projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
+            Rectangle screenRectangle = this.RectangleToScreen(this.ClientRectangle);
+            int titleHeight = screenRectangle.Top - this.Top;
+            int borderWidth = screenRectangle.Right - this.Right;
+
+            GlControlBounds = new Rectangle(glControl.Bounds.X + Bounds.X - borderWidth,
+                glControl.Bounds.Y + Bounds.Y + titleHeight,
+                glControl.Bounds.Width,
+                glControl.Bounds.Height);
         }
 
-        public void glControl_Paint(object sender, PaintEventArgs e)
+        private void glControl_Paint(object sender, PaintEventArgs e)
         {
             if (sender != null)
             {
                 glControl.MakeCurrent();
-
                 GL.ClearColor(Color4.MidnightBlue);
-                //GL.ClearColor(new Color4(0, 44, 25, 255));
-
                 GL.Clear(ClearBufferMask.ColorBufferBit);
-
                 glControl.SwapBuffers();
             }
         }
+
+        private void captureMouseCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (captureMouseCheckBox.Checked)
+            {
+                glControl.Focus();
+                Cursor.Hide();
+                Cursor.Clip = GlControlBounds;
+
+                INativeInput input = glControl.EnableNativeInput();
+                if (NativeInput == null)
+                {
+                    NativeInput = input;
+                    NativeInput.KeyUp += (e) =>
+                        {
+                            if (e.Key == OpenTK.Windowing.GraphicsLibraryFramework.Keys.Escape)
+                            {
+                                captureMouseCheckBox.Checked = false;
+                            }
+                        };
+                }
+            }
+
+            if (!captureMouseCheckBox.Checked)
+            {
+                glControl.DisableNativeInput();
+                glControl.Focus();
+                Cursor.Show();
+                Cursor.Clip = Rectangle.Empty;
+                front = new Vector3(0.0f, 0.0f, -1.0f);
+                position.Z = 30f;
+            }
+        }
     }
+    
 }
